ef445969862f223b8b5785e780d4218c141481b9
pacucam <pacucam@alumni.uv.es>
2026-02-07 18:29:37 +0100

    Perímetro rectángulo y cambio PI

--- Perim.R
+++ Perim.R
@@ -2,6 +2,10 @@ AreaCuadrado<-function(L){
   return(4*L)
 }
 
+LongitudCircunferencia <- function(R) {
+  return(2 * 3.141519 * R)
+}
+
 PerimetroRectangulo <- function(L, l) {
   return(2 * L + 2 * l)
 }
